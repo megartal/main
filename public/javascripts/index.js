05a6b1b9ae3a7fd83555765261e38d44cfc8eb4e
megartal <akbar.mirzaii@gmail.com>
2018-07-06 00:40:04 +0430

    first commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+    datePicker();
+
+    var results;
+
+    $('#q').keyup(function () {
+        $.ajax({
+            url: 'http://localhost:4000/api/city',
+            data: { city: $(this).val() },
+            success: function (data) {
+                $('.autocomplete-suggestions').children().remove();
+                data.forEach(function(element){
+                    $('.autocomplete-suggestions').append('<div class="autocomplete-suggestion" data-index="0"><small><strong>' + element.city + '</strong></small></div>');
+                });
+            }
+        })
+    });
+
+    $('#submit-mysearch').click(function () {
+        search = collectSearchData();
+        window.location.href = "http://localhost:3000/hotels";
+        $.ajax({
+            url: "http://localhost:4000/api/search",
+            type: 'POST',
+            data: search,
+            success: function (data) {
+                window.
+                data.forEach(function(element){
+                var s = '<p>salam '+ element.hotel_name +'</p>'
+                $('#results').append(s);
+              }); 
+            }
+        });
+    });
+
+    $('#hotel-search-form').on('mouseenter', '.autocomplete-suggestion', function () {
+        $(this).css("background-color", "#e4e6e8");
+        $(this).css("cursor", "pointer");
+    });
+
+    $('#hotel-search-form').on('mouseleave', '.autocomplete-suggestion', function () {
+        $(this).css("background-color", "white");
+        $(this).css("cursor", "pointer");
+    });
+
+    $('#hotel-search-form').on('click', '.autocomplete-suggestion', function () {
+        var city = $(this).text();
+        $('#q').val(city);
+        $('.autocomplete-suggestions').children().remove();
+    });
+
+    $('#new-search').click(function () {
+        $('#criteria').toggleClass('edit');
+    });
+
+});
+
+function createResult(element) {
+
+}
+
+function collectSearchData() {
+    var search = {};
+    search.city = $('#q').val();
+    search.from = $('#dateRangePicker').val();
+    search.to = $('#dateRangePickerEnd').val();
+    search.guest = $('#na').val();
+    search.rooms = $('#nr').val();
+    return search;
+}
+
+function datePicker() {
+    var night;
+    var isRtl = true;
+    var dateFormat = isRtl ? 'jYYYY/jMM/jDD' : 'YYYY/MM/DD';
+    var dateFrom = false ? moment("") : undefined;
+    var dateTo = false ? moment("") : undefined;
+    var $dateRanger = $("#dateRangePicker");
+
+    $dateRanger.daterangepicker({
+        clearLabel: 'Clear',
+        autoUpdateInput: !!(dateFrom && dateTo),
+        minDate: moment(),
+        autoApply: true,
+        opens: isRtl ? 'left' : 'right',
+        locale: {
+            separator: ' - ',
+            format: dateFormat
+        },
+        startDate: dateFrom,
+        endDate: dateTo,
+        jalaali: isRtl,
+        showDropdowns: true
+    }).on('apply.daterangepicker', function (ev, picker) {
+        night = picker.endDate.diff(picker.startDate, 'days');
+        if (night > 0) {
+            $(this).val(picker.startDate.format(dateFormat));
+            $('#dateRangePickerEnd').val(picker.endDate.format(dateFormat));
+        } else {
+            $(this).val('')
+        }
+    });
+}
+
---
+++ public/javascripts/keep.js
@@ -0,0 +1,134 @@
+var s = '<li class="hotel-card cpa-hotel-card   clearfix" id="'+ element.hotel_name +'" data-hotel-name="">' + 
+'<div class="cpa__hotel-card__container">' +
+    '<a href="" class="hotel-thumbnail cpa-hotel-thumbnail hotel-details-link" data-inlinetab="gallery" style="height: auto !important; display: block;">'+
+        '<div class="cpa-main-image-container">' +
+            '<img class="cpa-main-image" src="">' +
+            '<div class="cpa-main-image-cover">' +
+                '<span> More photos </span>' +
+                '<img class="cpa-more-photos-image" src="/images/more_photos.png">' +
+            '</div>' +
+        '</div>' +
+        '<img class="lazy-htile cpa-small-image" src="">'+
+        '<img class="lazy-htile cpa-small-image" src="">' +
+        '<img class="lazy-htile cpa-small-image" src="">' +
+    '</a>' +
+
+    '<div class="hotel-content cpa__hotel-card__content">'+
+        '<a href="" class="hotel-details hotel-details-link">' +
+        '</a>' +
+        '<div class="hotel-meta clearfix">' +
+            '<a href="" class="hotel-details hotel-details-link">' +
+            '</a>' +
+            '<div class="hotel-title-map-link">' +
+                '<a href="" class="hotel-details hotel-details-link">' +
+                    '<h3 data-icon="stars after" class="stars-4 hotel-name" data-stars="4">هتل امام خميني</h3>'+
+                '</a>' +
+                '<div class="hotel-distance-wrapper">' +
+                    '<a href="" class="hotel-details hotel-details-link">' +
+                        '<span data-icon="distance before" class="hotel-geo map-trigger" data-map-trigger="Hotel Tile" data-lat="" data-lon="">'+
+                            'مشاهده در نقشه'+
+                        '</span>'+
+                    '</a>'+
+                    '<div class="hotel_yet_another_semantic_name_wrapper">'+
+                        '<a href="" class="hotel-details hotel-details-link">'+
+                        '</a>'+
+                        '<a href="" class="hotel_policies hotel-details-link " data-inlinetab="facilities">'+
+                            '<span class="hotel_policy hotel_policy_wifi" data-icon="Internet_white before">Wi-Fi</span>'+
+                            '<span class="hotel_policy hotel_policy_parking" data-icon="Parking_positive before">پارکينگ</span>'+
+                        '</a>'+
+                    '</div>'+
+                    '<div>'+
+                    '</div>'+
+                '</div>'+
+                '<a href="" class="hotel-details hotel-details-link">'+
+                    '<div class="hotel-cpa-reviews">'+
+                        '<p>"باشگاه بدنسازي"</p>'+
+                        '<p>"استخر"</p>'+
+                    '</div>'+
+                '</a>'+
+                '<div class="hotel-deals">'+
+                    '<div class="hidden-prices hidden">'+
+                        '<div class="hotel-deals">'+
+                            '<a href="" target="_blank" class="hotel-deal-item-link ">'+
+                                '<div class="hotel-deal-item">'+
+                                    '<div class="room_column">'+
+                                        '<div class="deal_meta partner_meta">'+
+                                            '<div class="partner partner-logo" style="background-image:url(//www.skyscanner.net/images/websites/220x80/h_sa.png);"></div>'+
+                                        '</div>'+
+                                        '<div class="deal_meta room_meta">'+
+                                            '<div class="room_type">'+
+                                                'Room'+
+                                            '</div>'+
+                                            '<div class="room_policies">'+
+                                            '</div>'+
+                                        '</div>'+
+                                    '</div>'+
+                                    '<div class="hotel-deal-price price_column ">'+
+                                        '<div class="deal_meta price_meta price_format_total">'+
+                                            '<span class="price price_total price_full">'+
+                                                '£416'+
+                                            '</span>'+
+                                        '</div>'+
+                                        '<div class="deal_meta price_meta price_format_room_night">'+
+                                            '<span class="price price_total price_full">'+
+                                                '£84'+
+                                            '</span>'+
+                                        '</div>'+
+                                        '<div class="deal_meta cta_meta">'+
+                                            '<button class="hotel-cta-main" type="button">View deal</button>'+
+                                        '</div>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</a>'+
+                        '</div>'+
+                        '<div class="cpa-see-prices-bar-hidden">'+
+                            '<div class="cpa-see-prices-bar">'+
+                                '<span class="cpa-see-prices-from">'+
+                                    '1 قيمت شروع از'+
+                                    '<span class="cpa-lowest-price price_format_total"> 1،250،000 تومان </span>'+
+                                    '<span class="cpa-lowest-price price_format_room_night"> 250،000 تومان </span>'+
+                                '</span>'+
+                                '<button class="hotel-cta-main hotel-details-link cpa-see-prices-cta" type="button" data-inlinetab="prices"> مشاهده قيمت ها </button>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                    '<div class="cpa__hotel-card__deal_discover">'+
+                        '<a class="cpa__hotel-card__deal_discover__price hotel-details-link" data-inlinetab="prices" href="/hotels/hotel/Ibis+Tehran+Imam+Khomeini+International+Airport?idx=0&amp;id=136286039&amp;sd=2018-04-20&amp;ed=2018-04-25&amp;na=1&amp;nr=1&amp;q=Iran&amp;jbi=0&amp;tq=Iran&amp;eid=o_29475194&amp;trps=0&amp;trld=1#detail-providers">'+
+                            '<div class="cpa__hotel-card__deal_discover__see-prices">'+
+                                '1 قيمت شروع از'+
+                            '</div>'+
+                            '<div>'+
+                                '<div class="deal_meta price_meta price_format_total">'+
+                                    '<span class="price price_total price_full">'+
+                                        '1،250،000 تومان'+
+                                    '</span>'+
+                                '</div>'+
+                                '<div class="deal_meta price_meta price_format_room_night">'+
+                                    '<span class="price price_total price_full">'+
+                                        '250،000 تومان'+
+                                    '</span>'+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="price-description-msg">'+
+                                '<div class="price-per-room-msg" id="price-description-msg-total">'+
+                                    'قيمت کل براي 5 شب ،يک نفر،يک اتاق'+
+                                '</div>'+
+                                '<div class="price-per-room-msg" id="price-description-msg-room-night">'+
+                                    'قيمت براي هر اتاق براي يک نفر'+
+                                '</div>'+
+                            '</div>'+
+                        '</a>'+
+                        '<div class="hotel-cta-secondary hotel-details-link cpa__hotel-card__main-cta clearfix" data-inlinetab="prices">'+
+                            '<a class="deal_discover_to_details cpa__hotel-card__deal_discover_to_details" data-track-mp-usmp="true" href="/hotels/hotel/Ibis+Tehran+Imam+Khomeini+International+Airport?idx=0&amp;id=136286039&amp;sd=2018-04-20&amp;ed=2018-04-25&amp;na=1&amp;nr=1&amp;q=Iran&amp;jbi=0&amp;tq=Iran&amp;eid=o_29475194&amp;trps=0&amp;trld=1#detail-providers"'+
+                                'data-icon="downBlue after">'+
+                                '<span class="expand_details">مشاهده جزئيات</span>'+
+                                '<span class="collapse_details hidden">بستن جزئيات</span>'+
+                            '</a>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+    '</div>'+
+'</div>'+
+'</li>';
