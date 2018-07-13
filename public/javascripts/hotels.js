3339ef0fe28b9e293261fd3866e6b2c555828fa3
megartal <akbar.mirzaii@gmail.com>
2018-07-13 11:14:00 +0430

    range filter added and improved

--- public/javascripts/hotels.js
+++ public/javascripts/hotels.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    setDefultDate();
+    var city = '', star = 0, accomType = 'hotel', flag = true, range = 0, page = 1;
     datePicker();
     search();
 
@@ -53,123 +53,138 @@ $(document).ready(function () {
         $('#criteria').toggleClass('edit');
     });
 
+    //pagination
     $('.hbox').on('click', 'li', function () {
-        var currentPageNum = param('page');
         $(this).siblings().children().removeClass('selected');
         $(this).children().addClass('selected');
-        var newPageNum = $(this).children().text();
-        var url = location.href.replace("page=" + currentPageNum, "page=" + newPageNum);
-        document.location = url;
+        page = $(this).children().text();
+        search();
+        window.scrollTo(0, 0);
     });
 
     $('input[name="filter-price"]').on('change', function () {
         $('input[name="filter-price"]').not(this).prop('checked', false);
-        search('price', $(this).val());
+        if ($(this).prop('checked')) {
+            range = $(this).val();
+        } else {
+            range = 0;
+        }
+        search();
     });
 
     $('input[name="filter-category"]').on('change', function () {
         $('input[name="filter-category"]').not(this).prop('checked', false);
-        search('star', $(this).val());
+        if ($(this).prop('checked')) {
+            star = $(this).val();
+        } else {
+            star = 0
+        }
+        search();
     });
 
     $('input[name="filter-city"]').on('change', function () {
         $('input[name="filter-city"]').not(this).prop('checked', false);
-        search('city', $(this).val());
+        if ($(this).prop('checked')) {
+            city = $(this).val()
+        } else {
+            city = '';
+        }
+        search();
     });
 
     $('input[name="filter-accomodationtype"]').on('change', function () {
         $('input[name="filter-accomodationtype"]').not(this).prop('checked', false);
-        search('accomType', $(this).val());
+        if ($(this).prop('checked')) {
+            accomType = $(this).val();
+        } else {
+            accomType = 'hotel'
+        }
+        search();
     });
 
     $('#hotel-sort').on('change', function () {
-        search('sort');
+        search();
     });
+
+    function search(type, value) {
+        if (city == '')
+            city = param('q');
+
+        $.ajax({
+            url: "http://localhost:4000/api/search",
+            type: 'POST',
+            data: {
+                city: city, from: param('from'), to: param('to'), guest: param('na'), rooms: param('nr'),
+                sort: $('#hotel-sort').val(), page: page, star: star, type: accomType, range: range
+            },
+            success: function (data) {
+                if (data) {
+                    var template = $('#cartId').html();
+                    var dealTemplate = $('#dealId').html();
+                    var otaTemplate = $('#OTAId').html();
+                    populateChangedData(data);
+                    $('#results').children().remove();
+                    data.hotel_result.forEach(function (element) {
+                        var cardhtml = Mustache.render(template, element);
+                        var dealHtml = Mustache.render(dealTemplate, element);
+                        $('#results').append(cardhtml);
+                        hotelId = '#' + element.hotel_id;
+                        if (element.internet)
+                            $(hotelId + 'amenity').append('<span class="hotel_policy hotel_policy_wifi" data-icon="Internet_white before">Wi-Fi</span>');
+                        if (element.parking)
+                            $(hotelId + 'amenity').append('<span class="hotel_policy hotel_policy_parking" data-icon="Parking_positive before">Parking</span>');
+
+                        for (var i = 0; i < element.stars; i++) {
+                            $(hotelId + 'stars').append('<span style="font-size:100%;color:Orange;">&starf;</span>');
+                        }
+                        $(hotelId).append(dealHtml);
+                        element.ota_results.forEach(function (ota) {
+                            ota.hotel_id = element.hotel_id;
+                            var otaHtml = Mustache.render(otaTemplate, ota);
+                            $(hotelId + 'ota').append(otaHtml);
+                            ota.price_info.rooms.forEach(function (roomName) {
+                                $(hotelId + ota.ota.id).append('<div class="room_type">' + roomName + '</div>')
+                            });
+                        });
+                    });
+                } else {
+                    $('#results').children().remove();
+                    alert('there is no result');
+                }
+
+            }
+        });
+    }
+
+    function populateChangedData(data) {
+        $('.search-summary strong').text('');
+        $('.search-summary strong').append(data.result_num);
+        $('.search-summary em').text('');
+        $('.search-summary em').append($('#hotel-sort option:selected').text());
+        $('.pagination-summary em').text('');
+        $('.pagination-summary em').append(data.query.sort);
+        $('.hotel-criteria-destination').text('');
+        $('.hotel-criteria-destination').append(data.query.city);
+        $('.check-in').text('');
+        $('.check-in').append(data.query.from);
+        $('.check-out').text('');
+        $('.check-out').append(data.query.to);
+        $('.guests').text('');
+        $('.guests').append(data.query.guest + ' نفر ');
+        $('.rooms').text('');
+        $('.rooms').append(data.query.rooms + ' اتاق ');
+        $('.hbox').children().remove();
+        for (var i = 1; i <= Math.ceil((data.result_num / 10)); i++) {
+            if (i == page) {
+                $('.hbox').append('<li><button class="selected" data-page="' + i + '">' + i + '</button></li>');
+            } else {
+                $('.hbox').append('<li><button data-page="' + i + '">' + i + '</button></li>');
+            }
+        }
+    }
 
 });
 
-function search(type, value) {
-    var flag = true;
-    var city = '';
-    var star = 0;
-    var accomType = 'hotel';
-    var range = 0;
-    var page = param('page');
-    if (type == 'city') {
-        city = value;
-        page = 1;
-        flag = false;
-    } else {
-        city = param('q');
-    }
-    if (type == 'star') {
-        star = value;
-        page = 1;
-        flag = false;
-    }
-    if (type == 'accomType') {
-        accomType = value;
-        page = 1;
-        flag = false;
-    }
-    if (type == 'price') {
-        range = value;
-        page = 1;
-        flag = false;
-    }
-    if (type == 'sort') {
-        page = 1;
-        flag = false;
-    }
-
-
-    $.ajax({
-        url: "http://localhost:4000/api/search",
-        type: 'POST',
-        data: {
-            city: city, from: param('from'), to: param('to'), guest: param('na'), rooms: param('nr'),
-            sort: $('#hotel-sort').val(), page: page, star: star, type: accomType, range: range
-        },
-        success: function (data) {
-            if (data) {
-                var template = $('#cartId').html();
-                var dealTemplate = $('#dealId').html();
-                var otaTemplate = $('#OTAId').html();
-                populateChangedData(data);
-                if (flag)
-                    populateFixData(data);
-                $('#results').children().remove();
-                data.hotel_result.forEach(function (element) {
-                    var cardhtml = Mustache.render(template, element);
-                    var dealHtml = Mustache.render(dealTemplate, element);
-                    $('#results').append(cardhtml);
-                    hotelId = '#' + element.hotel_id;
-                    if (element.internet)
-                        $(hotelId + 'amenity').append('<span class="hotel_policy hotel_policy_wifi" data-icon="Internet_white before">Wi-Fi</span>');
-                    if (element.parking)
-                        $(hotelId + 'amenity').append('<span class="hotel_policy hotel_policy_parking" data-icon="Parking_positive before">Parking</span>');
-
-                    for (var i = 0; i < element.stars; i++) {
-                        $(hotelId + 'stars').append('<span style="font-size:100%;color:Orange;">&starf;</span>');
-                    }
-                    $(hotelId).append(dealHtml);
-                    element.ota_results.forEach(function (ota) {
-                        ota.hotel_id = element.hotel_id;
-                        var otaHtml = Mustache.render(otaTemplate, ota);
-                        $(hotelId + 'ota').append(otaHtml);
-                        ota.price_info.rooms.forEach(function (roomName) {
-                            $(hotelId + ota.ota.id).append('<div class="room_type">' + roomName + '</div>')
-                        });
-                    });
-                });
-            } else {
-                $('#results').children().remove();
-                alert('there is no result');
-            }
-
-        }
-    });
-}
 
 function populateFixData(data) {
     $('.hotel-criteria-destination').append(data.query.city);
@@ -179,26 +194,6 @@ function populateFixData(data) {
     $('.rooms').append(data.query.rooms + ' اتاق ');
 }
 
-function populateChangedData(data) {
-    $('.search-summary strong').text('');
-    $('.search-summary strong').append(data.result_num);
-    $('.search-summary em').text('');
-    $('.search-summary em').append($('#hotel-sort option:selected').text());
-    $('.pagination-summary em').text('');
-    $('.pagination-summary em').append(data.query.sort);
-    $('.hbox').children().remove();
-    for (var i = 1; i <= Math.ceil((data.result_num / 10)); i++) {
-        if (i == param('page')) {
-            $('.hbox').append('<li><button class="selected" data-page="' + i + '">' + i + '</button></li>');
-        } else {
-            $('.hbox').append('<li><button data-page="' + i + '">' + i + '</button></li>');
-        }
-    }
-}
-
-function processResults(data) {
-
-}
 
 
 
